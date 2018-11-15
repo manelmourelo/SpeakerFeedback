4b751d02fda72da14e5eb445cc4f10f40150cbd6
manelmourelo <manelmourelo.1@gmail.com>
2018-11-15 10:32:57 +0100

    Commit1

--- app/src/main/java/edu/upc/citm/android/speakerfeedback/MainActivity.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/MainActivity.java
@@ -12,7 +12,12 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textview;
     private String userId;
 
+    ListenerRegistration roomRegistration;
+    ListenerRegistration userRegistration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,17 +40,18 @@ public class MainActivity extends AppCompatActivity {
         textview = findViewById(R.id.textview);
 
         // Busquem a les preferències de l'app l'ID de l'usuari per saber si ja s'havia registrat
-        SharedPreferences prefs = getSharedPreferences("config", MODE_PRIVATE);
-        userId = prefs.getString("userId", null);
-        if (userId == null) {
+      //  SharedPreferences prefs = getSharedPreferences("config", MODE_PRIVATE);
+      //  userId = prefs.getString("userId", null);
+      //  if (userId == null) {
             // Hem de registrar l'usuari, demanem el nom
-            Intent intent = new Intent(this, RegisterUserActivity.class);
-            startActivityForResult(intent, REGISTER_USER);
-            Toast.makeText(this, "Encara t'has de registrar", Toast.LENGTH_SHORT).show();
-        } else {
+       //     Intent intent = new Intent(this, RegisterUserActivity.class);
+       //     startActivityForResult(intent, REGISTER_USER);
+       //     Toast.makeText(this, "Encara t'has de registrar", Toast.LENGTH_SHORT).show();
+       // } else {
             // Ja està registrat, mostrem el id al Log
-            Log.i("SpeakerFeedback", "userId = " + userId);
-        }
+       //     Log.i("SpeakerFeedback", "userId = " + userId);
+       // }
+        getOrRegisterUser();
     }
 
     @Override
@@ -87,4 +96,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private EventListener<DocumentSnapshot> roomListener = new EventListener<DocumentSnapshot>() {
+        @Override
+        public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+            if(e != null){
+                Log.e("SpeakerFeedback", "Error al rebre rooms/testroom", e);
+                return;
+            }
+            String name = documentSnapshot.getString("name");
+            setTitle(name);
+        }
+    };
+
+    private EventListener<QuerySnapshot> userListener = new EventListener<QuerySnapshot>() {
+        @Override
+        public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+            if(e != null){
+                Log.e("SpeakerFeedback", "Error al rebre usuaris dins d'un room", e);
+                return;
+            }
+            //textview.setText(String.format("Numuser %d", documentSnapshots.size()));
+            String nomsUsuaris = "";
+            for(DocumentSnapshot doc : documentSnapshots){
+                nomsUsuaris += doc.getString("name") + "\n";
+            }
+            textview.setText(nomsUsuaris);
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        roomRegistration = db.collection("rooms").document("testroom").addSnapshotListener(roomListener);
+
+        userRegistration = db.collection("users").whereEqualTo("room", "testroom").addSnapshotListener(userListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        roomRegistration.remove();
+    }
+
+    private void getOrRegisterUser(){
+        SharedPreferences prefs = getSharedPreferences("config", MODE_PRIVATE);
+        userId = prefs.getString("userId", null);
+        if (userId == null) {
+            // Hem de registrar l'usuari, demanem el nom
+            Intent intent = new Intent(this, RegisterUserActivity.class);
+            startActivityForResult(intent, REGISTER_USER);
+            Toast.makeText(this, "Encara t'has de registrar", Toast.LENGTH_SHORT).show();
+        } else {
+            // Ja està registrat, mostrem el id al Log
+            Log.i("SpeakerFeedback", "userId = " + userId);
+        }
+    }
+
 }
